f0d540f8428030a5a695d7086f69ffbfa51bab34
yiqu zhu <kotomineshiki@outlook.com>
2018-03-24 19:36:43 +0800

    最终版本
    包括简答题博客

---
+++ imgui/Assets/fdf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fdf : MonoBehaviour {
+
+    // Use this for initialization
+    void Awake() { Debug.Log(1); }
+
+    void Start() { Debug.Log(2); }
+
+    void Update() { Debug.Log(3); }
+
+    void FixedUpdate() { Debug.Log(4); }
+    private void LateUpdate()
+    {
+        Debug.Log(6);
+    }
+}
--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
-博客
+游戏实现博客
 https://blog.csdn.net/kotomineshiki/article/details/79678320
+简答题博客
+https://blog.csdn.net/kotomineshiki/article/details/79678795
